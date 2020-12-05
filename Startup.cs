055c934784d1eda9b7f0b666403b7bf37e138bf1
Christian LeVesque <christian@christianlevesque.io>
2020-12-05 14:41:47 -0600

    removed the Startup constructor

--- Startup.cs
+++ Startup.cs
@@ -15,13 +15,6 @@ namespace header_parser
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
-
-		public IConfiguration Configuration { get; }
-
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
