d436bbbceb29e7531d27145e6d5746662e66a552
Christian LeVesque <christian@christianlevesque.io>
2020-12-07 15:48:30 -0600

    Added missing </p>

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace header_parser
 				context.Response.Headers.Add("X-Application-Purpose", "FreeCodeCamp Request Header Parser Microservice");
 				context.Response.ContentType = "text/html";
 				context.Response.StatusCode = 200;
-				await context.Response.WriteAsync($"<p>Your software is {software}");
+				await context.Response.WriteAsync($"<p>Your software is {software}</p>");
 			});
 		}
 	}
