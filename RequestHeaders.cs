98d6e59b1db15f74f0dc38af1100de81ac726c42
Christian LeVesque <christian@christianlevesque.io>
2020-12-08 11:30:46 -0600

    Updated RequestHeaders.cs with JsonPropertyName attributes for each class property

--- RequestHeaders.cs
+++ RequestHeaders.cs
@@ -1,9 +1,16 @@
+using System.Text.Json.Serialization;
+
 namespace header_parser
 {
 	public class RequestHeaders
 	{
+		[JsonPropertyName("ipaddress")]
 		public string IpAddress { get; set; }
+		
+		[JsonPropertyName("software")]
 		public string Software { get; set; }
+		
+		[JsonPropertyName("language")]
 		public string Language { get; set; }
 	}
 }
