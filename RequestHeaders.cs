95eba4105615234c7760aa33f5fbfee11fc0c9c7
Christian LeVesque <christian@christianlevesque.io>
2020-12-08 11:05:34 -0600

    Added RequestHeaders data model

---
+++ RequestHeaders.cs
@@ -0,0 +1,9 @@
+namespace header_parser
+{
+	public class RequestHeaders
+	{
+		public string IpAddress { get; set; }
+		public string Software { get; set; }
+		public string Language { get; set; }
+	}
+}
