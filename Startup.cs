2093470fa7e854e3b1b0ae1912c808a4aeae4339
Christian LeVesque <christian@christianlevesque.io>
2020-12-11 15:10:55 -0600

    Added CORS support to the application

--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,26 @@ using System.Text.Json;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace header_parser
 {
 	public class Startup
 	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy("AllowAll", policy =>
+				{
+					policy.AllowAnyOrigin();
+					policy.AllowAnyMethod();
+					policy.AllowAnyHeader();
+				});
+			});
+		}
+		
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
@@ -15,6 +29,8 @@ namespace header_parser
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			app.UseCors("AllowAll");
 
 			app.Run(async context =>
 			{
